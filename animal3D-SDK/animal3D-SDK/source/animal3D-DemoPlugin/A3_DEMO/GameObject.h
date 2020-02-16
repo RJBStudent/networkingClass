bd109b3e39d47ab3fceebaeaedb34f8f2e57af49
361rjb <361rjb@gmail.com>
2020-02-16 17:22:10 -0500

    Gameobject render Function

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/GameObject.h
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/GameObject.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "animal3D/animal3D.h"
+#include"a3_DemoState.h"
 #include <string>
 
 class GameObject
@@ -21,6 +23,25 @@ public:
 	void setY(int y) { yPos = y; }
 	void setString(std::string newString) { myString = newString; }
 	void setRed(bool newIsRed) { isRed = newIsRed; }
+
+	void Render(a3_DemoState* const demoState)
+	{
+		a3f32 x = (a3f32)xPos;
+		a3f32 y = (a3f32)yPos;
+		a3f32 height = (a3f32)demoState->windowHeight;
+		a3f32 width = (a3f32)demoState->windowWidth;
+		if (isRed)
+		{
+			a3textDraw(demoState->text, (x/ width),  (y / height), -1, 1, 0, 0, 1, "%s", myString);
+
+		}
+		else
+		{
+			a3textDraw(demoState->text, (x/ width), (y / height), -1, 0, 0, 1, 1, "%s", myString);
+		}
+	}
+	//void Update(a3_DemoState* const demoState);
+
 
 private:
 	int xPos = 0;
